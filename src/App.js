70fcdf674d1743a46a3158484127c116397f03cb
CORP\blodovic <benedicte.lodovici@sogeti.com>
2018-01-10 17:12:26 +0100

    Home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './styles/images/logo.svg';
+import home from './styles/images/home.jpg';
+import alliances from './styles/images/alliances.PNG';
 import './styles/css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <header className="App-header">
+              <ul className="App-header-menu">
+                  <li>ACCUEIL</li>
+                  <li>NOTRE HISTOIRE</li>
+                  <li>INFOS</li>
+                  <li>NOUS TROUVER</li>
+                  <li>RSVP</li>
+              </ul>
+          </header>
+        <div className="App-content"> { /* changer pour que ça soit dynamique dans le App-content */ }
+            <img alt="" src={home}  />
+        </div>
+          <footer className="App-footer">
+              <div className="App-footer-content">MARIAGE <img alt="" src={alliances}  /> 1.1.1001</div>
+          </footer>
       </div>
     );
   }
