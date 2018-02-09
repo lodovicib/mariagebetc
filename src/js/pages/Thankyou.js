6f24045d00f6f5d56f2ca89f6f59559625dfaf9d
CORP\blodovic <benedicte.lodovici@sogeti.com>
2018-02-09 16:25:42 +0100

    Page remerciements

--- src/js/pages/Thankyou.js
+++ src/js/pages/Thankyou.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import Container from 'muicss/lib/react/container';
 import '../../styles/css/style.css';
+import background from '../../styles/images/background_5.jpg';
 
 class Thankyou extends Component {
-
     render() {
         return (
-            <div className="Thankyou">
+            <div>
+                <img src={background} style={{width: '100%', position: 'absolute', 'z-index': 1}} />
                 <div className="App-contain">
-                    <h1>Remerciements</h1>
+                    <div className="Thankyou">
+                        <h1>Remerciements</h1>
+                        <div className="content-bloc">
+                            <Container>
+                                Merci tousa toussa
+                            </Container>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
